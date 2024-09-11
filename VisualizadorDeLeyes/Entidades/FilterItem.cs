d1a638fc0a8be5ff5b4c3db6569a7ebf8843331a
franciscocampillo <francisco.campillo@hotmail.com>
2024-09-11 21:25:13 +0200

    se añaden filtros por título capítulo sección.

---
+++ VisualizadorDeLeyes/Entidades/FilterItem.cs
@@ -0,0 +1,10 @@
+﻿namespace VisualizadorDeLeyes.Entidades
+{
+    public class FilterItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+        public List<FilterItem> Children { get; set; } = new List<FilterItem>();
+    }
+}
